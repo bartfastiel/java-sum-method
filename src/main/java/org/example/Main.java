d0651977dcdcf625e4bbe15d06a531d16d77a741
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-25 16:37:07 +0200

    Implement java method that calculates sum of two ints

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sum(3, 4));
+    }
+
+    private static int sum(int summandA, int summandB) {
+        return summandA + summandB;
     }
 }
